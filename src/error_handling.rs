919989cd3cf7a9844b6ed0073aa70b8d898c90fc
HellOwhatAs <xjq701229@outlook.com>
2023-10-14 13:39:28 +0800

    commit

---
+++ src/error_handling.rs
@@ -0,0 +1,41 @@
+fn recoverable() {
+    {
+        fn thrower() -> Result<(), i32> {
+            Err(123)
+        }
+        fn caller() -> Result<String, i32> {
+            thrower()?;
+            Ok("Ok".to_string())
+        }
+        match caller() {
+            Ok(msg) => println!("Ok: {msg}"),
+            Err(e) => println!("Err: {e}"),
+        }
+    }
+    {
+        fn noner() -> Option<()> {
+            None
+        }
+        fn caller() -> Option<String> {
+            noner()?;
+            Some("Not None".to_string())
+        }
+        match caller() {
+            Some(msg) => println!("Some: {msg}"),
+            None => println!("None"),
+        }
+    }
+    {
+        fn last_char_of_first_line(text: &str) -> Option<char> {
+            text.lines().next()?.chars().last()
+        }
+        match last_char_of_first_line("Hello, world\nHow are you today?") {
+            Some(c) => println!("last char of first line is {c}"),
+            None => println!("Not any line"),
+        }
+    }
+}
+
+pub fn main() {
+    recoverable();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-mod guessing_name;
+mod guessing_num;
 mod common_programming_concepts;
 mod ownership;
 mod structs;
 mod enums;
 mod packages_crates_and_modules;
 mod common_collections;
+mod error_handling;
 
 fn main() {
     println!("Hello World!");
-    // guessing_name::main();
+    guessing_num::main();
     // common_programming_concepts::main();
     // ownership::main();
     // structs::main();
     // enums::main();
     // packages_crates_and_modules::main();
-    common_collections::main();
+    // common_collections::main();
+    error_handling::main();
 }
