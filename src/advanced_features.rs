f5ec2dcd2f9e6c4c8198134a2609ab9e6a22ee5a
HellOwhatAs <88815487+HellOwhatAs@users.noreply.github.com>
2023-10-19 18:58:38 +0800

    commit

--- src/advanced_features.rs
+++ src/advanced_features.rs
@@ -75,6 +75,118 @@ fn unsafe_rust() {
 }
 
 fn advanced_traits() {
+    {
+        use std::ops::Add;
+        {
+            #[derive(Debug)]
+            struct Point<T> {
+                x: T,
+                y: T
+            }
+            impl<T> Add<&Point<T>> for Point<T> 
+                where T: for<'a> Add<&'a T, Output = T>
+            {
+                type Output = (T, T);
+                fn add(self, rhs: &Point<T>) -> Self::Output {
+                    (self.x + &rhs.x, self.y + &rhs.y)
+                }
+                
+            }
+            let a = Point { x: 20, y: 30 };
+            let b = Point { x: -2, y: 99 };
+            println!("{:?}", a + &b);
+            println!("{:?}", b);
+        }
+        {
+            #[derive(Debug)]
+            struct A(i32);
+            #[derive(Debug)]
+            struct B(i32);
+            impl Add<B> for A {
+                type Output = A;
+                fn add(self, rhs: B) -> Self::Output {
+                    A(self.0 + rhs.0)
+                }
+            }
+            impl Add<&B> for A {
+                type Output = B;
+                fn add(self, rhs: &B) -> Self::Output {
+                    B(self.0 + rhs.0)
+                }
+            }
+            let a = A(70) + B(5);
+            let b = A(70) + &B(5);
+            println!("{:?}, {:?}", a, b);
+        }
+    }
+    {
+        trait MyToString {
+            fn to_string(&self) -> String {
+                "Default MyToString".to_string()
+            }
+        }
+        struct A(i32);
+        impl std::fmt::Display for A {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "A({})", self.0)
+            }
+        }
+        impl MyToString for A {
+            fn to_string(&self) -> String {
+                format!("[MyToString]A({})", self.0)
+            }
+        }
+        impl A {
+            fn to_string(&self) -> String {
+                format!("[method]A({})", self.0)                
+            }
+        }
+        let a = A(10);
+        println!(
+            "{}, {}, {}, {}",
+            a,
+            a.to_string(),
+            MyToString::to_string(&a),
+            ToString::to_string(&a)
+        );
+        println!("{}", <A as MyToString>::to_string(&a));
+    }
+    {
+        trait AbsString: ToString + std::fmt::Display {
+            fn to_absstring(&self) -> String {
+                println!("{}", self);
+                format!("|{}|", self.to_string())                
+            }
+        }
+        impl AbsString for i32 {}
+        println!("{}", 123.to_absstring());
+    }
+    {
+        struct Wrapper<T>(Vec<T>);
+        impl<T> std::ops::Deref for Wrapper<T> {
+            type Target = Vec<T>;
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+        impl<T> std::ops::DerefMut for Wrapper<T> {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                &mut self.0
+            }
+        }
+        impl<T: std::fmt::Display> std::fmt::Display for Wrapper<T> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "[{}]", self.iter().map(T::to_string).collect::<Vec<String>>().join(", "))
+            }
+        }
+        let mut a = Wrapper(vec![1, 2, 3, 4]);
+        a.push(0);
+        a.sort_by_key(|elem| i32::abs(3 - elem));
+        println!("{}", a);
+    }
+}
+
+fn advanced_types() {
 
 }
 
@@ -82,4 +194,5 @@ fn advanced_traits() {
 pub fn main() {
     unsafe_rust();
     advanced_traits();
+    advanced_types();
 }
