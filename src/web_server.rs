066ceb501b81051a01f74e8afd8c2fdd8d77f19e
HellOwhatAs <xjq701229@outlook.com>
2023-10-20 20:12:58 +0800

    commit

--- src/web_server.rs
+++ src/web_server.rs
@@ -1,5 +1,7 @@
 mod single_threaded;
+mod multithreads;
 
 pub fn main() {
-    single_threaded::main();
+    // single_threaded::main();
+    multithreads::main();
 }
--- src/web_server/multithreads.rs
+++ src/web_server/multithreads.rs
@@ -1,12 +1,16 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{BufReader, BufRead, Write};
+use threadpool::ThreadPool;
 
 #[allow(dead_code)]
 pub fn main() {
     let listener = TcpListener::bind("127.0.0.1:8001").unwrap();
+    let pool = ThreadPool::new(16);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(move || {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -17,6 +21,8 @@ fn handle_connection(mut stream: TcpStream) {
         .take_while(|x| !x.is_empty())
         .collect::<Vec<_>>()
         .join("\n");
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
 
     let status_line = "HTTP/1.1 200 OK";
     let html = format!(include_str!("./index.html"), http_request);
